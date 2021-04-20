ee91f901b6f52a6db25a676a95972d1717ecfd0c
CamDes95 <80051843+CamDes95@users.noreply.github.com>
2021-04-20 17:31:44 +0200

    Update streamlit_App.py

--- Streamlit App/streamlit_App.py
+++ Streamlit App/streamlit_App.py
@@ -11,7 +11,7 @@ import bce_dice_loss
 import cloudImage
 import os
 import cv2
-from efficientnet.tfkeras import EfficientNetB4
+from efficientnet.tfkeras import EfficientNetB4 # Pour charger le modèle
 import time
 
 
@@ -36,7 +36,7 @@ st.markdown(
     f'''
     <style>
         .sidebar .sidebar-content {{
-            width:1000px;
+            width:700px;
         }}
     ''', unsafe_allow_html=True)  # Width de la sidebar
     
@@ -60,12 +60,12 @@ if menu_sel == "Présentation du projet":
     st.write("""Les scientifiques de l'institut Max Plack de Météorologie développent et analysent 
              des modèles très complexes simulant les processus géophysiques ayant lieues dans 
              l'atmosphère. Les nuages superficiels jouent un rôle déterminant sur le climat de la 
-             Terre, et sont difficiles à interpréter et représenter dans les modèles climatiques.""")
-    st.write(""" Les chercheurs désirent améliorer l’identification de ces types de nuages, ce qui 
-             permettrait de construire de meilleures projections et fournir des pistes de
+             Terre, et sont difficiles à interpréter et représenter dans les modèles climatiques. Les chercheurs désirent améliorer 
+             l’identification de ces types de nuages, ce qui permettrait de construire de meilleures projections et fournir des pistes de
              compréhension vis à vis du changement climatique global.""")
     st.write("""Au total, quatre formations nuageuses ont été identifiées par les scientifiques : Fish, Flower, Gravel et Sugar.  """)
-    
+    st.write("Afin de répondre à la problèmatique posée par les scientifiques de l'Institut Max Planck, nous avons construit un modèle de Deep Learning capable d'identifier les quatre formes de nuages.")
+
     st.subheader("Description des formations nuageuses :")
     
     if st.checkbox("Fish"):
@@ -91,9 +91,6 @@ La différence entre les formations de type Gravel et Sugar est parfois difficil
         left.image("Sugar.png", width=700)
         st.write("""Zones étendues de nuages très fins ; en cas de fort débit, ils peuvent former de fines «
 veines » ou « plumes » (nuages dentritiques).""")
-
-
-    st.write("Afin de répondre à la problèmatique posée par les scientifiques de l'Institut Max Planck, nous avons construit un modèle de Deep Learning capable d'identifier les quatre formes de nuages.")
 
 
 
@@ -130,14 +127,14 @@ if menu_sel == "Dataset":
     
     fig, ax = plt.subplots(figsize=(6, 6))
     x = list([fish, flower, gravel, sugar])
-    plt.pie(x, labels=['Fish', 'Flower', 'Gravel', 'Sugar'], autopct='%1.1f%%')
+    plt.pie(x, labels=['Fish', 'Flower', 'Gravel', 'Sugar'], autopct = '%1.1f%%')
     ax.set_title('Cloud Types');
-    st.subheader("Répartition des formes nuageuses dans notre dataset")
+    st.subheader("Répartition des formes nuageuses")
     st.pyplot(fig)
     
     df = pd.DataFrame([['Sugar',sugar],['Flower',flower],['Gravel',gravel],['Fish',fish]], columns= ['Pattern', 'Count'])
 
-    if st.checkbox("Afficher le nombre d'images par formes nuageuses identifiées"):
+    if st.checkbox("Afficher le nombre d'images par forme nuageuse identifiée"):
         st.dataframe(df)
 
     if st.checkbox("Afficher les dimensions du dataset"):
@@ -161,47 +158,81 @@ if menu_sel == "Dataset":
   
 if menu_sel == "Modélisation":
     st.markdown('<style>.stylex{color: black; font-size:35px}, </style>', unsafe_allow_html=True)
-    st.markdown('<p class="stylex">Modelisation Encoder-Decoder </p>', unsafe_allow_html=True)
-
-    st.markdown('<style>.style2{color: black; font-size:25px}, </style>', unsafe_allow_html=True)
-    st.markdown('<p class="style2">Schéma type du processus à implémenter. </p>', unsafe_allow_html=True)
+    st.subheader("Segmentation d'image")
+    st.text(" ")
+    st.markdown('<style> .text{color: black; font-size:22px}</style>', unsafe_allow_html=True)
+    st.markdown("""Nous allons développer un modèle de Deep Learning en utilisant des réseaux de neurones convolutifs (CNN) et des méthodes de Transfer Learning. 
+                """,unsafe_allow_html=True)
+    
+    st.markdown('Schéma type du processus à implémenter :', unsafe_allow_html=True)
     st.image('demarche.png', width=600)
 
-    st.markdown('<style> .text{color: black; font-size:22px}</style>', unsafe_allow_html=True)
-    st.markdown('<p class="text">Nous allons développer un modèle de Deep Learning en utilisant le Transfer Learning et un réseau de neurone convolutif (CNN),  qui va détecter les quatre types de formations nuageuses.</p>',unsafe_allow_html=True)
-    st.text("")
-    st.image('cnn.png', width=600)
+    st.markdown("""Afin de classifier chacune des formes nuageuses, nous utiliserons les techniques de segmentation d'images à l'aide d'un Auto-Encoder (Encoder + Decoder).
+                L'ensemble des démarches réalisées sont schématisées sur l'image ci-dessous.""",unsafe_allow_html=True)
+
+
+    st.markdown('<style>.style2{color: black; font-size:25px}, </style>', unsafe_allow_html=True)
+    st.text(" ")
+
+    left, middle, right=st.beta_columns([8,1,1])
+    left.image('cnn.png', width=600)
 
     if st.checkbox("Image inputs"):
-        st.write("Nous avons 5546 images de taille : (1400*2100). Les images utilisées en entrée du modèle ont été formatées en 224*224")
+        st.info("Nous travaillons sur un dataset de 5546 images de taille 1400x2100. Les images utilisées en entrée du modèle ont été formatées en 224x224.")
 
     if st.checkbox("Data Generator"):
-        st.write("Un générateur de données permet de charger l’ensemble des données dans le réseau de neurones via des mini-lots d’images pour éviter de saturer la RAM ou Le GPU de l'ordinateur.")
+        st.info("Un générateur de données permet de charger l’ensemble des données dans le réseau de neurones via des mini-lots d’images pour éviter de saturer la RAM ou Le GPU de l'ordinateur.")
 
     if st.checkbox("Image batchs"):
-        st.write("Les lots d'images qui vont être traités par notre modèle (batch size = 16.")
+        st.info("Les lots d'images qui vont être traités par notre modèle (batch size = 16).")
 
     if st.checkbox("Data Augmentation"):
-        st.write("Les techniques d’augmentation de données permettent d’accroître la diversité de notre jeu d’entraînement en appliquant des transformations aléatoires sur les images.")
+        st.info("Les techniques d’augmentation de données permettent d’accroître la diversité de notre jeu d’entraînement en appliquant des transformations aléatoires sur les images.")
 
     if st.checkbox("Encoder"):
-        st.write("Un modèle CNN pré-entraîné (UNet, EfficientNet, ResNet, DenseNet) qui va compresser l'information.")
+        st.info("Un modèle CNN pré-entraîné utilisé par Transfer Learning (EfficientNet, ResNet, DenseNet) qui va extraire les composantes des images et compresser l'information.")
 
     if st.checkbox("Decoder"):
-        st.write("un modèle CNN qui va convertir la sortie de l’encodeur et reconstruire l’image sous forme de classes.")
+        st.info("un modèle CNN qui va convertir la sortie de l’encodeur et reconstruire l’image sous forme de classes.")
 
     if st.checkbox("Classification"):
-        st.write("La couche de sortie correspond à une classification multi-classes par pixels. La fonction d'activation 'Softmax' permet de récupèrer les probabilités d'appartenance des pixels à chaque classe.")
-
-    st.markdown("<h2>Entraînement</h2>", unsafe_allow_html=True)
-    st.markdown("<p>Nous avons entraînés plusieurs modèles afin de déterminer le plus stable et possèdant la meilleure performance. Les résultats de ces entraînements sont indiqués dans la partie suivante </p>", unsafe_allow_html=True)
+        st.info("La couche de sortie correspond à une classification multi-classes par pixels. La fonction d'activation 'Softmax' permet de récupèrer les probabilités d'appartenance des pixels à chaque classe.")
     
 
 ######################### RESULTATS ##########################
 
 if menu_sel == "Résultats":  
-    st.title("Résultats")
-    st.markdown("Présentation des résultats") 
+    st.subheader("Entraînement")
+    st.markdown("""<p>Nous avons entraînés plusieurs modèles en faisant varier les hyper-paramètres afin de déterminer celui obtenant la meilleure performance. 
+                Les résultats de ces entraînements sont détaillés dans le tableau ci-dessous. </p>""", unsafe_allow_html=True)
+    st.markdown("""Liste des hyper-paramètres utilisés :""")
+    
+    if st.checkbox("Poids"):
+        st.info("L’utilisation de modèles pré-entraînés comme encodeur permet de récupérer les poids et d’extraire précisément les caractéristiques des images. Dans notre projet, nous avons utilisés les poids de la base de données ImageNet.")
+
+    if st.checkbox("Fonction de perte"):
+        st.info("Nous allons ici utiliser la fonction de perte dice loss (indice de Sørensen-Dice) qui permet de mesurer la similarité entre deux échantillons.")
+
+    if st.checkbox("Taux d'apprentissage"):
+        st.info("Il permet de règler le rythme de mise à jour des poids à la fin de chaque batch. Nous testerons ici plusieurs taux d'apprentissage de 1e-2 à 5e-5 en utilisant l'optimiseur Adam.")
+
+    if st.checkbox("Batch size"):
+        st.info("En raison de la mémoire disponible sur notre matériel pour l'entraînement, nous avons utilisé un batch size de 16.")
+
+    if st.checkbox("Fonction d'activation"):
+        st.info("Nous avons utilisé la fonction d’activation softmax qui génère un vecteur représentant les distributions de probabilité d’une liste de résultats potentiels. Elle est souvent utilisée dans le cas des classifications multi-classes.")
+
+    if st.checkbox("Nombre d'epochs"):
+        st.info("Nous avons tout d'abord entraînés plusieurs modèles sur 10 epochs, puis sélectionnés ceux présentant les meilleurs performances afin de les réentraîner à l'aide d'un EarlyStopping.")
+
+    st.markdown("Le tableau ci-dessous présente les résultats des deux meilleurs modèles entraînés.")
+    
+    st.image("final iteration.png")
+    
+    st.markdown("Au vu des performances, nous allons présenter dans la partie suivante les prédictions du modèle utilisant l'encodeur EfficientNetB2.")
+    
+    if st.checkbox("Liste et résultats des modèles testés"):
+        st.image("iterations.png")
     
 
 ######################### PREDICTION ##########################
@@ -353,27 +384,6 @@ if menu_sel == "Application sur de nouvelles données":
     
     model = load_model("model_efficientnetb2_encoder_weights_imagenet_lr0.001_20epochs_DataAugmentEncoderFreeze_07_0.57.h5", custom_objects={'loss': bce_dice_loss}, compile=False)
     
-    
-    images_list = ["Images d'entraînement", "Images test"]
-    images_sel = st.radio("", images_list, index=0, key=None)
-    
-    if images_sel == "Images d'entraînement":
-        directory = "reduced_train_images_224/"
-        opt = st.selectbox(
-        "Choix de l'image",
-        df_train.FileName.unique())
-        path = directory + opt
-        st.image(path, channels="BGR", width = 341)
-
-    if images_sel == "Images test":
-        directory1 = "reduced_test_images_224/"
-        opt1 = st.selectbox(
-        "Choix de l'image",
-        test_imgs)
-        path1 = directory1 + opt1
-        st.image(path1, channels="BGR", width = 341)
-    
-    
     uploaded_file = st.file_uploader("", type=["jpg","png","jpeg"])
 
     if uploaded_file is not None:
@@ -419,6 +429,7 @@ if menu_sel == "Application sur de nouvelles données":
     
     
 # Ajouter bouton pressoir repo git, Dataset et FAQ 
+
 st.text("")
 left_column, right_column = st.sidebar.beta_columns(2)
 pressed = left_column.button('GitHub Repository')
@@ -435,36 +446,19 @@ linkedin_profile = ["https://fr.linkedin.com/in/camille-desjardin-82842b122",
                     "https://fr.linkedin.com/in/toufik-saddik-b2131640",
                     "https://fr.linkedin.com/in/luceglin" ]
 
-st.sidebar.markdown("## About us")
+st.sidebar.markdown("## About")
 st.sidebar.info("Projet segmentation de régions nuageuses - Avril 2021  \n\n"
                 "__________________  \n"
-                "Luc EGLIN {}   \n"
-                "Camille DESJARDIN {}   \n"
-                "Toufik SADDIK {}".format(linkedin_profile[2],linkedin_profile[0],linkedin_profile[1]))
+                "Luc EGLIN   \n"
+                "{}   \n"
+                "Camille DESJARDIN   \n"
+                "{}   \n"
+                "Toufik SADDIK   \n"
+                "{}   \n".format(linkedin_profile[2],linkedin_profile[0],linkedin_profile[1]))
 
 
 
 #FAQ
 expander = st.beta_expander("FAQ")
 expander.write("Here you could put in some really, really long explanations...")    
-    
-
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
